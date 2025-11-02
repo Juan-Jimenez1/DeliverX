7fdad23f779dae780161a7749d778a01bc50352d
Juan-Jimenez1 <juanj.jimenezb@uqvirtual.edu.co>
2025-11-02 18:22:03 -0500

    commit
    se agregaron atributos a las clases shipment y address

--- DeliverX/src/main/java/co/edu/uniquindio/poo/deliverx/model/Address.java
+++ DeliverX/src/main/java/co/edu/uniquindio/poo/deliverx/model/Address.java
@@ -1,4 +1,10 @@
 package co.edu.uniquindio.poo.deliverx.model;
 
 public class Address {
+    private String addressId;
+    private String street;
+    private String city;
+    private String type;
+    private String latitude;
+    private String longitude;
 }
--- DeliverX/src/main/java/co/edu/uniquindio/poo/deliverx/model/Shipment.java
+++ DeliverX/src/main/java/co/edu/uniquindio/poo/deliverx/model/Shipment.java
@@ -1,4 +1,13 @@
 package co.edu.uniquindio.poo.deliverx.model;
 
+import java.time.LocalDate;
+
 public class Shipment {
+    private String idShipment;
+    private Address origin;
+    private Address destination;
+    private double weight;
+    private DeliveryMan deliveryMan;
+    private Customer customer;
+    private LocalDate dateTime;
 }
